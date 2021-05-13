a0234f8b358b46bd63d029bfac6cf9accac97d2e
MaxiFly29 <dimaidanya79@gmail.com>
2021-05-13 21:16:27 +0300

    Добавьте файлы проекта.

---
+++ ASD2_5/ASD2_5.cpp
@@ -0,0 +1,90 @@
+﻿#include <functional>
+#include <forward_list>
+#include <vector>
+#include <algorithm>
+
+#include "profile.h"
+
+using namespace std;
+
+template <typename Type, typename Hasher = hash<Type>>
+class HashSet {
+public:
+    using BucketList = forward_list<Type>;
+
+public:
+    explicit HashSet(
+        size_t num_buckets,
+        const Hasher& hasher = {}
+    ) : un_map(num_buckets) {}
+
+    void Add(const Type& value) {
+        if (!Has(value))
+            un_map[Hasher()(value) % un_map.size()].push_front(value);
+    }
+    bool Has(const Type& value) const {
+        auto hassh = Hasher()(value) % un_map.size();
+        return find(begin(un_map[hassh]), end(un_map[hassh]), value) != un_map[hassh].end();
+    }
+    void Erase(const Type& value) {
+        auto hassh = Hasher()(value) % un_map.size();
+        if (Has(value))
+            un_map[hassh].remove(value);
+    }
+
+    vector<Type> orderedSequence() const {
+        vector<Type> v;
+        for (const auto& buck : un_map) {
+            for (auto val : buck) {
+                v.push_back(val);
+            }
+        }
+        sort(begin(v), end(v));
+        return v;
+    }
+    
+private:
+    vector<BucketList> un_map;
+};
+
+int main() {
+    vector<int> rvec(1e7);
+    for (int& x : rvec)
+        x = rand() % 1'000'000;
+    for (int i = 0; i < 10; i++) {
+        HashSet<int> hs(1e7);
+        {
+            LOG_DURATION("method Add - " + to_string(i + 1));
+            for (int j = 0; j < 1e7; j++) {
+                hs.Add(rvec[j]);
+            }
+        }
+
+        {
+            LOG_DURATION("method Find (exists) - " + to_string(i + 1));
+            for (int j = 0; j < 1e7; j++) {
+                auto f = hs.Has(rvec[j]);
+            }
+        }
+
+        {
+            LOG_DURATION("method Find (not exists) - " + to_string(i + 1));
+            for (int j = 0; j < 1e6; j++) {
+                auto f = hs.Has(rvec[j] + 1e7);
+            }
+        }
+
+        {
+            LOG_DURATION("method orderedSequence - " + to_string(i + 1));
+            auto res = hs.orderedSequence();
+        }
+
+        {
+            LOG_DURATION("method Erase - " + to_string(i + 1));
+            for (int j = 0; j < 1e7; j++) {
+                hs.Erase(rvec[j]);
+            }
+        }
+    }
+    return 0;
+}
